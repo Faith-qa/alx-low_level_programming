c370db63bd716b78bd167a09e36ec67e9bb7ba15
Faith-qa <atienofaith12@gmail.com>
2020-09-18 22:24:12 +0300

    while loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,7 +4,6 @@
  *main - entrypoint for prints all possible different combination of two digits
 *Return: Always 0 (Success)
 */
-
 int main(void)
 {
 int a = 0;
@@ -13,14 +12,15 @@ while (a < 10)
 {
 b = 0;
 while (b < 10)
-if ((!(a == b))&&a < b)
+{
+if ((!(a == b)) &&a < b)
 {
 putchar('0' + a);
 putchar('0' + b);
-if (!(a == 8 && b == 9))
+if ((!(a == 8 && b == 9)))
 {
-putchar(',');
 putchar(' ');
+putchar(',');
 }
 }
 b++;
