4fe527421841b17f4e0175ae68eeaca1b7d4db9c
Faith-qa <atienofaith12@gmail.com>
2020-09-18 17:37:01 +0300

    runs file simultaneously

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <time.h>
-/*
-*prints all possible different combination of two digits
-*Return: 0
+/**
+ *main - entrypoint for prints all possible different combination of two digits
+*Return: Always 0 (Success)
 */
 
 int main(void)
 {
 int a = 0;
 int b;
-int c;
 while (a < 10)
 {
 b = 0;
 while (b < 10)
+if ((!(a == b))&&a < b)
 {
 putchar('0' + a);
 putchar('0' + b);
-if (!(a == 9 && b == 9))
+if (!(a == 8 && b == 9))
 {
 putchar(',');
 putchar(' ');
