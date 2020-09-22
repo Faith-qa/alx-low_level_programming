b4554971567160b527587dd3ac490a090847968d
Faith-qa <atienofaith12@gmail.com>
2020-09-22 21:53:37 +0300

    checks and finds lower case letters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
 /**
- *int _islower(int c); - checks for lowercase characters
- *
+ *int _islower - checks for lower case letters
+ *@c: interger to check
  * Return: (1) if lowercase and  (0) if its not
- *
  */
-int _islower(int c)
+int _islower(int c);
 {
 if (c >= 'a' && c <= 'z')
 {
