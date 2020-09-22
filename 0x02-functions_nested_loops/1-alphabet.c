1316698324292656f054dcfbd6ebefaef8479218
Faith-qa <atienofaith12@gmail.com>
2020-09-22 14:49:14 +0300

    prints alphabet in lower case followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *main - print the alphabet (void)
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+void print_alphabet(void);
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+return (0);
+}
