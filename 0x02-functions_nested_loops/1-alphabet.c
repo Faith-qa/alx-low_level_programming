ed6f8ded61c324599897cd447dce1a370bfb4d32
Faith-qa <atienofaith12@gmail.com>
2020-09-22 15:14:43 +0300

    prints the alphabet followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,9 @@
  *main - print the alphabet (void)
  *Return: Always 0 (success)
  */
+void print_alphabet(void);
 int main(void)
 {
-void print_alphabet(void);
 char c;
 for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
