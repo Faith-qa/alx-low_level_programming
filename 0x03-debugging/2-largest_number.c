4c5eb707f3407f0b264ca2954a5fa44843618fe9
Faith-qa <atienofaith12@gmail.com>
2020-09-23 19:51:26 +0300

    fixing code to give the right input

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,17 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && b > c)
+if (a >= b && a >= c)
 {
-largest = c;
+largest = a;
 }
-else if (b > a && a > c)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
 else
 {
-largest = a;
+largest = c;
 }
 
 return (largest);
