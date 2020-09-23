21ae53bd0dc82084c561492d97588547a34bd2ef
Faith-qa <atienofaith12@gmail.com>
2020-09-23 12:52:29 +0300

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,13 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-*positive_or_negative - classify as + or -
-*@i: integer
 * Return: 0
 */
 
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
 return (0);
