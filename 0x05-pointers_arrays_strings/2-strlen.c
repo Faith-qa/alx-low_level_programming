52cc20201c358246a7d8ca3614173fdce81b08dc
Faith-qa <atienofaith12@gmail.com>
2020-09-29 20:59:24 +0300

    returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_strlen - returns the length of a string
+ *@s: integer entered
+ *Return: void
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; ++i)
+{
+}
+return (i);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -14,6 +14,7 @@ void print_square(int size);
 void print_triangle(int size);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 
 #endif
