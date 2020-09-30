17192cba670950e59177edf0381612e1789788d1
Faith-qa <atienofaith12@gmail.com>
2020-09-30 11:56:39 +0300

    edits

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,16 +6,17 @@
  */
 void print_rev(char *s)
 {
-int x; /**forward*/
-int y; /**reverse/backward*/
-int l ; /**length*/
-for (x = 0; s[x] != '\0'; x++)
-{
-l = x;
-}
-y = 0;
-for (; l >= y; l--)
-{
-_putchar(s[l]);
-}
+	int x; /**forward*/
+	int y; /**reverse/backward*/
+	int l ; /**length*/
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		l = x;
+	}
+	y = 0;
+	for (; l >= y; l--)
+	{
+		_putchar(s[l]);
+	}
 }
