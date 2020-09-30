a38c3b3c060372fce3229ac832cd850118941ef4
Faith-qa <atienofaith12@gmail.com>
2020-09-30 12:46:09 +0300

    function that reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,29 +2,25 @@
 #include <stdio.h>
 /**
  *rev_string - reverses a string
- *@s: character integer entered
+ *@s: integer entered
  *Return: void
  */
-int last char(char *s)
-{
-int len = 0;
-while (s[len] != '\0')
-len++;
-return (len);
-}
-
 void rev_string(char *s)
 {
-int len, i, half;
-i = 0;
-len = lastChar(s);
-half = len / 2;
-char temp;
-while (half--)
+int a, b, temp;
+/* a = start */
+/*b = end */
+
+a = 0;
+while (s[a] != '\0')
 {
-trmp = s[len - i - 1];
-s[len - i - 1] = s[i];
-s[i] = temp;
-i++;
+a++;
+}
+a--;
+for (b = 0; a > b; b++, a--)
+{
+temp = s[a];
+s[a] = s[b];
+s[b] = temp;
 }
 }
