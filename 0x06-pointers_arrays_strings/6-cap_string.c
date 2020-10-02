55b7a62d0f83f122a36ab8c4bdd457e0a1b7f62d
Faith-qa <atienofaith12@gmail.com>
2020-10-02 21:14:12 +0300

    capitalizing words in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,33 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * cap_string - capitalize all words in a string
- * @s: string to be  capitalized
+ * cap_string - Capitalizes all words of a string.
  *
- * Return: Void
+ * @s: s is the world to be analyzed
+ *
+ * Return: Always 0.
  */
-
 char *cap_string(char *s)
 {
-int i;
-i = 0;
+	int i = 0;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-if ((s[i] >= 97) && (s[i] <= 122))
-{
-s[i] -= 32;
-}
-if ((s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!'
-|| s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')'
-|| s[i] == '{' || s[i] ==  '}' || s[i] == ' ' || s[i] == '\n'
-|| s[i] == '\t') && s[i] >= 97 && s[i] <= 122)
-{
-s[i] -= 32;
-}
-}
-return (s);
+	while (s[i] != '\0')
+	{
+		if (s[0] <= 122 && s[0] >= 97)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[i] == 32 || s[i] == 46 || s[i] == '\t' ||
+		    s[i] == '\n' || s[i] == 44 || s[i] == 59 ||
+		    s[i] == '!' || s[i] == '?' || s[i] == '(' ||
+		    s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] <= 122 && s[i + 1] >= 97)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+		i++;
+	}
+	return (s);
 }
