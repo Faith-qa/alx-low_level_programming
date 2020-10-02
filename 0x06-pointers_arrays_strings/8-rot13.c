25f19bf9210260f1200c9ba9253e5f032808b160
Faith-qa <atienofaith12@gmail.com>
2020-10-02 21:30:13 +0300

    encodding a string using rot 13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+/**
+* rot13 - encodes a string using rot13.
+* @str: the string to be encoded.
+*
+* Return: A pointer to the encoded string.
+*/
+
+char *rot13(char *str)
+{
+	int a = 0;
+	int b;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+	while (str[a])
+	{
+		for (b = 0; b < 52; b++)
+		{
+			if (str[a] == alphabet[b])
+			{
+				str[a] = rot13key[b];
+				break;
+			}
+		}
+
+		a++;
+	}
+
+	return (str);
+}
