ea27b63e5d95b9955d8951a62ebb3d9c6aa29d03
Faith-qa <atienofaith12@gmail.com>
2020-10-05 20:55:49 +0300

    edits

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,18 +6,18 @@
  *@src: source of memory
  *@dest: memory destination
  *@n: number of bytes from memory area
- *@Return: dest
+ *
+ *Return: memory destination (dest)
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  unsigned int i;
-  i = 0;
-  while (i < n)
-    {
-      dest[i] = src[i];
-      i++;
-    }
-  return (dest);
+unsigned int i;
+i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
 }
-  
+return (dest);
+}
