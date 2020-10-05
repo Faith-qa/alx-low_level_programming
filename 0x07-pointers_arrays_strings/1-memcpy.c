420426112d16b535ec4f16832a9f518ef9808127
Faith-qa <atienofaith12@gmail.com>
2020-10-05 18:04:28 +0300

    copies memory area without the *_memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_memcpy - copies memory area
+ *@src: source of memory
+ *@dest: memory destination
+ *@n: number of bytes from memory area
+ *@Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+  unsigned int i;
+  i = 0;
+  while (i <= n)
+    {
+      dest[i] = src[i];
+      i++;
+    }
+  return (dest);
+}
+  
