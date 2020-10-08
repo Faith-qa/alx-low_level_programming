3ec40c03ed0fd093c2c67222fcf6a1dc5da62f44
Faith-qa <atienofaith12@gmail.com>
2020-10-08 19:43:13 +0300

    string comparison

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -8,12 +8,11 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-  if (*s1 == '\0' && *s2 == '\0')
-      return (1);
-  
-  if (*s1 == *s2)
-      return (wildcmp(s1 + 1, s2 + 1));
-  if (*s2 == '*')
-    return (wildcmp(s1, s2 + 1) || wildcmp (s1 + 1, s2));
-  return (0);
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+if (*s2 == '*')
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+return (0);
 }
