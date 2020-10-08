2ff46abf40441be2a60a96d1fd0acdf5e6883618
Faith-qa <atienofaith12@gmail.com>
2020-10-08 11:37:04 +0300

    edits

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -3,5 +3,10 @@
 
 void _puts_recursion(char *s);
 int _putchar(char c);
-
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int sqrt_value(int a, int b);
 #endif
