49b0eebe76367b7f634e6d9b7e1dad7c52bc93d2
Faith-qa <atienofaith12@gmail.com>
2020-10-11 21:18:09 +0300

    adds 2 or more digits

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - prints sum of two numbers
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Description: prints sum of two numbers
+ * Return: Always(0) Success
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int sum;
+	int zero = 0;
+	char error[10] = "Error";
+
+
+	if (argc == 1)
+	{
+		printf("%d\n", zero);
+		return (0);
+	}
+
+	for (x = 0; x < argc; x++)
+	{
+		if (x > 0)
+		{
+			if (atoi(argv[x]) == 0)
+			{
+				printf("%s\n", error);
+				return (1);
+			}
+
+			sum += atoi(*(argv + x));
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
