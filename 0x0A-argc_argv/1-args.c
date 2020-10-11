4a81accdcc0182db028bb3ee0ac500aa2f527c59
Faith-qa <atienofaith12@gmail.com>
2020-10-11 06:27:21 +0300

    prints a number followed by a new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - prints number of arguments passed
+ *@argc: first parameter
+ *@argv: second parameter
+ *DESCRIPTION: prints number of arguments
+ *Return: Always (0) Success
+ */
+
+int main(int argc, char **argv)
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+argv[i] = argv[i];
+}
+printf("%d\n", i - 1);
+return (0);
+}
+  
