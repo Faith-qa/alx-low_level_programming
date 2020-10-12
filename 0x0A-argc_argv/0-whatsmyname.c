f03084ac1322d7e1ba9692b2cf8c130d978be301
Faith-qa <atienofaith12@gmail.com>
2020-10-12 14:38:32 +0300

    edits

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,12 +11,13 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-i = 0;
-while (i < argc)
-{
-printf("[%s]\n", argv[0]);
-i++;
-}
-return (0);
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[0]);
+		i++;
+	}
+	return (0);
 }
