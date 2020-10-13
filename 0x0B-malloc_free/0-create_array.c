3f48c622dc76d7a3c1f9e12413b727775e42c943
Faith-qa <atienofaith12@gmail.com>
2020-10-13 21:03:03 +0300

    edits

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,10 +11,10 @@
 
 char *create_array(unsigned int size, char c)
 {
-  char *p = malloc(size);
-  if (size == 0 || p == 0)
-    return (0);
-  while (size--)
-    p[size] = c;
-  return (p);
+char *p = malloc(size);
+if (size == 0 || p == 0)
+return (0);
+while (size--)
+p[size] = c;
+return (p);
 }
