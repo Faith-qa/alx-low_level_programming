0d6ea95be0e32f6a1078b9565ce2124ae86a2d79
Faith-qa <atienofaith12@gmail.com>
2020-10-20 18:50:28 +0300

    edits

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,7 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-void init_dog(struct dog *d, char *name, float *age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 char *_strdup(char *str);
 dog_t *new_dog(char *name, float age, char *owner);
