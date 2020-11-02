6542caf80ee3774764228ebc47c7cdcf0d4df671
Faith-qa <atienofaith12@gmail.com>
2020-11-02 20:45:58 +0300

    prints string before main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *  liebre_tortuga -function that executes before main
+ **/
+void liebre_tortuga(void)
+{
+	char *string1, *string2;
+
+	string1 = "You're beat! and yet, you must allow,\n";
+	string2 = "I bore my house upon my back!\n";
+	printf("%s%s", string1, string2);
+}
