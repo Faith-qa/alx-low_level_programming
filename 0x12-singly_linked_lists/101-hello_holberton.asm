8184b17d0298297874d507c3e5f97d6804a729c6
Faith-qa <atienofaith12@gmail.com>
2020-11-02 21:19:51 +0300

    64-bit program in assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+global main
+	section .text
+	
+main:
+	mov rdi, 1		;to the standard output
+	mov rsi, message	;message to print
+	mov rdx, 17     	;size of text to print
+	mov rax, 1		;write
+	syscall
+
+	mov eax, 60
+	xor rdi, rdi
+	syscall
+message:
+	db "Hello, Holberton", 10
