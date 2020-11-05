813cb1b12061ade70e56eeac81133fc082218393
Faith-qa <atienofaith12@gmail.com>
2020-11-05 21:52:58 +0300

    betty file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,12 +8,12 @@
  */
 void print_binary(unsigned long int n)
 {
-  if (n == 0)
-    _putchar('0');
-  else
-    {
-      if (n >> 1)
-	print_binary(n >> 1);
-      _putchar((n & 1) + '0');
-    }
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
 }
