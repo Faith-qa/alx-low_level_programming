e60aeab62e0d4c9bf188104a4234a3746808340c
Faith-qa <atienofaith12@gmail.com>
2020-11-06 21:37:06 +0300

    checks for endianness

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -12,4 +12,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int _putchar(char c);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
 #endif
