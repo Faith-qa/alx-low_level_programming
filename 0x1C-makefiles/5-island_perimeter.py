368d212c71f3b37f2c8ba9f355f45288d795d124
Faith-qa <atienofaith12@gmail.com>
2021-03-17 20:52:57 +0300

    corrected errors

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -18,12 +18,13 @@ def island_perimeter(grid):
     edges = 0
     size = 0
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-            if (j > 0 and grid[i - 1] == 1):
-                edges += 1
-            if (i > 0 and grid[i - 1][j] == 1):
+    while (width < 100 and height < 100):
+        for i in range(height):
+            for j in range(width):
+                if grid[i][j] == 1:
+                    size += 1
+                if (j > 0 and grid[i - 1] == 1):
                     edges += 1
-    return size * 4 - edges * 2
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+                    return (size * 4) + (edges * 4)
